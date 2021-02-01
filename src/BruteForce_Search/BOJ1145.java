cc03943bc7826e56c7123547e84aa70c4bfae5fc
user <gusdn85554@naver.com>
2021-02-01 16:06:31 +0900

    BOJ 1145

---
+++ src/BruteForce_Search/BOJ1145.java
@@ -0,0 +1,40 @@
+package BruteForce_Search;
+
+//BOJ 1145 적어도 대부분의 배수
+
+/*
+
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1145 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int arr[] = new int[5];
+
+        for(int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 1;
+
+        while(true) {
+            int cnt = 0;
+            for (int j = 0; j < 5; j++) {
+                if (result % arr[j] == 0)
+                    cnt++;
+            }
+            if (cnt >= 3) {
+                System.out.println(result);
+                return;
+            }
+            result++;
+        }
+    }
+}
