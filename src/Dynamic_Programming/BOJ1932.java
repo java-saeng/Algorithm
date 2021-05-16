0a3fb1c155967d6cdfe4d9cf170b0d1aa921f9f7
user <gusdn85554@naver.com>
2021-05-16 16:14:00 +0900

    BOJ 1932

---
+++ src/Dynamic_Programming/BOJ1932.java
@@ -0,0 +1,44 @@
+package Dynamic_Programming;
+
+//BOJ 1932 정수 삼각형
+
+/*
+ https://c-king.tistory.com/233
+ */
+
+import java.io.*;
+import java.util.*;
+
+class BOJ1932{
+    static int size;
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
+
+         size = Integer.parseInt(st.nextToken());
+
+         int triangle[][] = new int[size][size];
+         int count[][] = new int[size][size];
+
+         for(int i = 0; i < size; i++){
+             st = new StringTokenizer(br.readLine());
+             for(int j = 0; j < i+1; j++){
+                 triangle[i][j] = Integer.parseInt(st.nextToken());
+             }
+         }
+
+         count[0][0] = triangle[0][0];
+         int max = Integer.MIN_VALUE;
+         for(int i = 1; i < size; i++){
+             for(int j = 0; j <= i; j++){
+                 if(j == 0) count[i][j] = count[i-1][j] + triangle[i][j];
+                 else if(i == j) count[i][j] = count[i-1][j-1] + triangle[i][j];
+                 else{
+                     count[i][j] = Math.max(count[i-1][j-1] + triangle[i][j], count[i-1][j] + triangle[i][j]);
+                 }
+                 max = Math.max(max, count[i][j]);
+             }
+         }
+         System.out.print(max);
+    }
+}
